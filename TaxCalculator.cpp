eddb9f4a9ceb1df1b301c02291575e75537dbc71
Daniel-Zhi <30090168@cc.peralta.edu>
2026-01-28 17:49:22 +0000

    First draft

---
+++ TaxCalculator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    const double rate = 0.075;
+    double price;
+    double sales_tax;
+    double total;
+    cout << "Please enter the price of your item: ";
+    cin >> price;
+    sales_tax = price * rate;
+    total = price + sales_tax;
+    cout << fixed << setprecision(2) << "Your original price is " << price <<"\nYour sales tac amount is: " << sales_tax << "\nYour final total cost is: " << total;
+}
